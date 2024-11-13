1099ae0bd4fff547ab486b8bb29dc47e313234f7
Abhijith Pratheesh <147461848+Abhijith314@users.noreply.github.com>
2024-11-14 00:48:04 +0530

    Update macro_processor.c

--- OS MPMC SS/Macro Processor/macro_processor.c
+++ OS MPMC SS/Macro Processor/macro_processor.c
@@ -18,54 +18,54 @@ while(strcmp("END",la)!=0) {
     fprintf(f3,"%s\t%s\n",la,opnd);
     fscanf(f1,"%s%s%s",la,mne,opnd);
     while(strcmp(mne,"MEND")!=0) {
-    if(opnd[0]=='&'){
-    sprintf(pos1,"%d",pos);
-    strcpy(pos2,"?");
-    strcpy(opnd,strcat(pos2,pos1));
-    pos++;
-    }
-    else {
-    fprintf(f3,"%s\t%s\n",mne,opnd);
-    }
-    fprintf(f3,"%s\t%s\n",mne,opnd);
-    fscanf(f1,"%s%s%s",la,mne,opnd);
+        if(opnd[0]=='&'){
+            sprintf(pos1,"%d",pos);
+            strcpy(pos2,"?");
+            strcpy(opnd,strcat(pos2,pos1));
+            pos++;
+        }
+        else {
+            fprintf(f3,"%s\t%s\n",mne,opnd);
+        }
+        fprintf(f3,"%s\t%s\n",mne,opnd);
+        fscanf(f1,"%s%s%s",la,mne,opnd);
     }
     fprintf(f3,"%s",mne);
     }
     else{
-    rewind(f2);
-    fscanf(f2,"%s",name);
-    if(strcmp(name,mne)==0){
-    printf("%s\n",opnd);
-    len=strlen(opnd);
-    for(i=0;i<len;i++){
-    if(opnd[i]!=','){
-    fprintf(f4,"%c",opnd[i]);
-    printf("%c\n",opnd[i]);
-    }
-    else{
-    fprintf(f4,"\n");
-    }
-    }
-    rewind(f3);
-    rewind(f4);
-    fscanf(f3,"%s %s",mne1,opnd1);
-    fprintf(f5,".\t%s\t%s\n",mne1,opnd);
-    fscanf(f3,"%s %s",mne1,opnd1);
-    while(strcmp(mne1,"MEND")!=0) {
-    if(opnd1[0]=='?'){
-    fscanf(f4,"%s",arg);
-    fprintf(f5,"-\t%s\t%s\n",mne1,arg);
-    }
-    else {
-    fprintf(f5,"-\t%s\t%s\n",mne1,arg);
-    }
-    fscanf(f3,"%s %s",mne1,opnd1);
-    }
-    }
-    else{
-    fprintf(f5,"%s\t%s\t%s\n",la,mne,opnd);
-    }
+        rewind(f2);
+        fscanf(f2,"%s",name);
+        if(strcmp(name,mne)==0){
+            printf("%s\n",opnd);
+            len=strlen(opnd);
+            for(i=0;i<len;i++){
+                if(opnd[i]!=','){
+                    fprintf(f4,"%c",opnd[i]);
+                    printf("%c\n",opnd[i]);
+                }
+                else{
+                    fprintf(f4,"\n");
+                }
+            }
+            rewind(f3);
+            rewind(f4);
+            fscanf(f3,"%s %s",mne1,opnd1);
+            fprintf(f5,".\t%s\t%s\n",mne1,opnd);
+            fscanf(f3,"%s %s",mne1,opnd1);
+            while(strcmp(mne1,"MEND")!=0) {
+                if(opnd1[0]=='?'){
+                    fscanf(f4,"%s",arg);
+                    fprintf(f5,"-\t%s\t%s\n",mne1,arg);
+                }
+                else {
+                    fprintf(f5,"-\t%s\t%s\n",mne1,arg);
+                }
+                fscanf(f3,"%s %s",mne1,opnd1);
+            }
+        }
+        else{
+            fprintf(f5,"%s\t%s\t%s\n",la,mne,opnd);
+        }
     }
     fscanf(f1,"%s%s%s",la,mne,opnd);
 }
